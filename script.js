d4eb987a173fad8c3e276562e3004e98445044b9
HIM7707 <singhhimank400@gmail.com>
2024-04-10 13:54:40 +0530

    initial Files

---
+++ script.js
@@ -0,0 +1,23 @@
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+      Montserrat : [ 'Montserrat', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
