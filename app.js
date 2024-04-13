35c07a330575ddd0835c70fb525526b3c0b208f4
HIM7707 <singhhimank400@gmail.com>
2024-04-13 16:04:49 +0530

    added dynamic hover in top-bar

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const nodemailer = require('nodemailer');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ;
 
 
 // Access environment variables
